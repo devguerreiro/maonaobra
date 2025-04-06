a0daad6c67e77e875d55eb9893689306370b0cbf
Luis <luiscvlh11@gmail.com>
2025-04-06 07:44:24 -0300

    build: :green_heart: corrige deploy vercel

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -1,18 +1,5 @@
-type Row = {
-  Prestador: string;
-  Especialidade: string;
-  Desempenho: string;
-};
-
-type Data = {
-  data: Array<Row>;
-  currentPage: number;
-  totalPage: number;
-};
-
 export async function GET() {
-  const response = await fetch(process.env.GOOGLE_SHEET_URL, {
+  return await fetch(process.env.GOOGLE_SHEET_URL, {
     method: "GET",
   });
-  return (await response.json()) as Data;
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,34 +1,41 @@
 "use client";
 
-import Form from "./components/Form";
+// import Form from "./components/Form";
 
-export default async function Page() {
-  async function getJobs() {
-    const response = await fetch(process.env.GOOGLE_SHEET_URL, {
-      method: "GET",
-    });
-    return (await response.json()) as Data;
-  }
+// type Row = {
+//   Prestador: string;
+//   Especialidade: string;
+//   Desempenho: string;
+// };
 
-  const response = await getJobs();
+// type Data = {
+//   data: Array<Row>;
+//   currentPage: number;
+//   totalPage: number;
+// };
 
-  const data = response.data;
-
-  const availableJobs = new Set(data.map((job) => job.Especialidade));
-
-  function onSubmit(job: string) {}
-
-  return (
-    <div className="container w-screen h-screen flex flex-col justify-center items-center gap-4">
-      <p className="text-center text-balance">
-        Selecione a especialidade que deseja buscar prestadores
-      </p>
-      <Form
-        jobs={Array.from(availableJobs)}
-        onSubmit={(values) => {
-          onSubmit(values.job);
-        }}
-      />
-    </div>
-  );
+export default function Page() {
+  // async function getJobs() {
+  //   const response = await fetch(process.env.GOOGLE_SHEET_URL, {
+  //     method: "GET",
+  //   });
+  //   return (await response.json()) as Data;
+  // }
+  // const response = await getJobs();
+  // const data = response.data;
+  // const availableJobs = new Set(data.map((job) => job.Especialidade));
+  // function onSubmit(job: string) {}
+  // return (
+  //   <div className="container w-screen h-screen flex flex-col justify-center items-center gap-4">
+  //     <p className="text-center text-balance">
+  //       Selecione a especialidade que deseja buscar prestadores
+  //     </p>
+  //     <Form
+  //       jobs={Array.from(availableJobs)}
+  //       onSubmit={(values) => {
+  //         onSubmit(values.job);
+  //       }}
+  //     />
+  //   </div>
+  // );
 }
