28c35da092d3f5ab851a81a954d0217d9196dd17
Luis <luiscvlh11@gmail.com>
2025-04-05 21:09:43 -0300

    feat: :sparkles: busca os jobs com base nos dados cadastrados da planilha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,33 @@
-"use client";
+import Form from "./components/Form";
 
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+type Job = {
+  Prestador: string;
+  Especialidade: string;
+  Desempenho: string;
+};
 
-import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
+type Data = {
+  data: Array<Job>;
+  currentPage: number;
+  totalPage: number;
+};
 
-const formSchema = z.object({
-  job: z.string().uuid({
-    message: "Selecione uma especialidade",
-  }),
-});
-
-type FormSchema = z.infer<typeof formSchema>;
-
-export default function Home() {
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      job: "",
-    },
-  });
-
-  function getJobs() {
-    return ["pedreiro", "mestre de obra"];
+export default async function Page() {
+  async function getJobs() {
+    const response = await fetch(process.env.GOOGLE_SHEET_URL, {
+      method: "GET",
+    });
+    return (await response.json()) as Data;
   }
 
-  const jobs = getJobs();
-
-  function handleSubmit(values: FormSchema) {
-    console.log(values);
-  }
+  const jobs = new Set((await getJobs()).data.map((job) => job.Especialidade));
 
   return (
     <div className="container w-screen h-screen flex flex-col justify-center items-center gap-4">
       <p className="text-center text-balance">
         Selecione a especialidade que deseja buscar prestadores
       </p>
-      <Form {...form}>
-        <form className="space-y-6" onSubmit={form.handleSubmit(handleSubmit)}>
-          <FormField
-            control={form.control}
-            name="job"
-            render={({ field }) => (
-              <FormItem>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger className="w-[180px]">
-                      <SelectValue placeholder="Especialidade" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {jobs.map((job) => (
-                      <SelectItem key={job} value={job}>
-                        {job}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full">
-            Buscar
-          </Button>
-        </form>
-      </Form>
+      <Form jobs={Array.from(jobs)} />
     </div>
   );
 }
